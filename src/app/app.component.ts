502e1e7778f28b5bc40b7ada47887759dc67e9ac
Sam Bulatov <stylesam@yandex.ru>
2018-12-15 17:21:19 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.styl']
+})
+export class AppComponent {
+  title = 'acedia-blog';
+}
